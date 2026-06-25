c6e78a3334432b8d6328e00b1d0866d5c6743955
Hibiki0417 <ashimine1184@gmail.com>
2026-06-25 20:07:14 +0900

    店舗詳細ページを追加

--- shops/views.py
+++ shops/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Shop
 
 
 def shop_list(request):
     shops = Shop.objects.all()
     return render(request, "shops/shop_list.html", {"shops": shops})
+
+def shop_detail(request,pk):
+    shop = get_object_or_404(Shop,pk=pk)
+    return render(request, "shops/shop_detail.html", {"shop":shop} )
