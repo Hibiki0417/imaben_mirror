3caf80010b638177b4e44022e44c70ec755781b4
Hibiki0417 <ashimine1184@gmail.com>
2026-07-23 04:51:25 +0900

    店舗管理画面に店舗情報を表示

--- manager/views.py
+++ manager/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.views.generic import TemplateView 
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from shops.models import Shop
 # Create your views here.
 class ManagerLogin(LoginView):
     template_name = "manager/manager_login.html"
 
 class ManagerDashboard(LoginRequiredMixin, TemplateView):
     template_name = "manager/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["shop"] = Shop.objects.first()
+        return context
